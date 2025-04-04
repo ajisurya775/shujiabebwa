58232faaf4f503b3963f798b7df9ebb5077d1e40
Your Name <ajisurya331@gmail.com>
2025-04-04 11:39:10 +0700

    install laravel filament and create resource or menu

--- app/Models/HomeService.php
+++ app/Models/HomeService.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Str;
 
 class HomeService extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $guarded = ['id'];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
 }
